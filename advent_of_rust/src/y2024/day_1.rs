2ba40829a55f77139aeca706d352dfdd67c6a023
sebostien <sebostien@proton.me>
2024-12-01 13:09:14 +0100

    2024 Day 1

--- advent_of_rust/src/y2024/day_1.rs
+++ advent_of_rust/src/y2024/day_1.rs
@@ -1,4 +1,4 @@
-use anyhow::anyhow;
+use std::collections::HashMap;
 
 use crate::Solution;
 
@@ -7,22 +7,47 @@ pub fn get_solution() -> Solution<u32, u32> {
         date: (2024, 1),
         part_1: Box::new(part_1),
         part_2: Box::new(part_2),
-        answer: (0, 0),
+        answer: (2970687, 23963899),
     }
 }
 
 fn part_1(input: &str) -> anyhow::Result<u32> {
-    Ok(input
+    let (mut ls, mut rs) = input
+        .trim()
         .lines()
         .map(|line| {
-            let mut chars = line.chars().filter_map(|c| c.to_digit(10));
-            let a = chars.next().unwrap();
-            let b = chars.last().unwrap_or(a);
-            a * 10 + b
+            let mut chars = line.trim().split("   ");
+            let a = chars.next().unwrap().parse::<u32>().unwrap();
+            let b = chars.next().unwrap().parse::<u32>().unwrap();
+            (a, b)
         })
-        .sum())
+        .fold((Vec::new(), Vec::new()), |(mut ls, mut rs), (l, r)| {
+            ls.push(l);
+            rs.push(r);
+            (ls, rs)
+        });
+
+    ls.sort_unstable();
+    rs.sort_unstable();
+
+    Ok(ls.into_iter().zip(rs).map(|(l, r)| l.abs_diff(r)).sum())
 }
 
 fn part_2(input: &str) -> anyhow::Result<u32> {
-    Err(anyhow!("Not Done!"))
+    let (ls, rs) = input
+        .trim()
+        .lines()
+        .map(|line| {
+            let mut chars = line.trim().split("   ");
+            let a = chars.next().unwrap().parse::<u32>().unwrap();
+            let b = chars.next().unwrap().parse::<u32>().unwrap();
+            (a, b)
+        })
+        .fold((Vec::new(), HashMap::new()), |(mut ls, mut rs), (l, r)| {
+            ls.push(l);
+            *rs.entry(r).or_insert(0) += 1;
+            (ls, rs)
+        });
+
+    Ok(ls.into_iter().map(|i| rs.get(&i).unwrap_or(&0) * i).sum())
 }
